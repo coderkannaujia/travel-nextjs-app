b4b77c305ea1c8576e6841fb8ae306b0c03395e4
coderkannaujia <kumamit482000@gmail.com>
2024-02-09 23:18:43 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
     <>
       <Navbar />
       <Service />
-     <Desination />
+      <Desination />
       <BookTrip />
       <Testimonials />
       <Logos />
